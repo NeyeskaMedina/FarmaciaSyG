2be9fbc23ab7ae2177700415ddb2e51b4ea0ef3f
Neyeska Medina <neyeska.go@gmail.com>
2024-05-30 22:50:07 -0400

    Reduciendo codigo de plugins

--- client/src/components/Facebook/Facebook.jsx
+++ client/src/components/Facebook/Facebook.jsx
@@ -3,46 +3,25 @@ import React from 'react'
 export const Facebook = () => {
     return (
         <>
-            <div>
             <div 
-                id="comp-irnm7zcf" 
-                className="kxyFWi comp-irnm7zcf"
+                // style="vertical-align: bottom; width: 484px; height: 575px;"
             >
-                <div 
-                    className="fb-page fb_iframe_widget" 
-                    data-href="http://www.facebook.com/farmaciasyg" 
-                    data-width="484" 
-                    data-height="575" 
-                    data-show-posts="true" 
-                    data-hide-cover="false" 
-                    data-show-facepile="true" 
-                    data-adapt-container-width="true" 
-                    data-testid="fb-page-element" 
-                    fb-xfbml-state="rendered" 
-                    fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=304553036307597&amp;container_width=0&amp;height=575&amp;hide_cover=false&amp;href=http%3A%2F%2Fwww.facebook.com%2Ffarmaciasyg&amp;locale=es_ES&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=true&amp;width=484"
+                <iframe 
+                    name="fafc420f2a4bb3458" 
+                    width="340px" 
+                    height="500px" 
+                    data-testid="fb:page Facebook Social Plugin" 
+                    title="fb:page Facebook Social Plugin" 
+                    // frameborder="0" 
+                    allowtransparency="true" 
+                    allowfullscreen="true" 
+                    scrolling="no" 
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" 
+                    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffarmaciasyg&tabs=timeline%2Cmessages%2Cevents&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
+                    className="" 
+                    // style="border: none; visibility: visible; width: 484px; height: 575px;"
                 >
-                        <span 
-                            // style="vertical-align: bottom; width: 484px; height: 575px;"
-                        >
-                            <iframe 
-                                name="fafc420f2a4bb3458" 
-                                width="340px" 
-                                height="500px" 
-                                data-testid="fb:page Facebook Social Plugin" 
-                                title="fb:page Facebook Social Plugin" 
-                                // frameborder="0" 
-                                allowtransparency="true" 
-                                allowfullscreen="true" 
-                                scrolling="no" 
-                                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" 
-                                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffarmaciasyg&tabs=timeline%2Cmessages%2Cevents&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
-                                className="" 
-                                // style="border: none; visibility: visible; width: 484px; height: 575px;"
-                            >
-                            </iframe>
-                        </span>
-                    </div>
-                </div>
+                </iframe>
             </div>
         </>
     )
